b101d2bd0708106e0e7ffcfd9353cfbc0000b251
Gz1st <dmitro1208@gmail.com>
2024-11-23 16:12:45 +0200

    fix

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
-    $('.menu-button').on('click', function() {
-        $('.menu-x').toggleClass('menu_active');
+document.addEventListener("DOMContentLoaded", function() {
+    const menuButton = document.querySelector('.menu-button');
+    const closeMenuBtn = document.querySelector('.close-menu-btn');
+    const menu = document.querySelector('.menu');
+
+    // Відкриття меню
+    menuButton.addEventListener('click', () => {
+        menu.classList.add('active'); // Додає клас для відкриття меню
+    });
+
+    // Закрити меню
+    closeMenuBtn.addEventListener('click', () => {
+        menu.classList.remove('active'); // Видаляє клас для закриття меню
     });
 });
